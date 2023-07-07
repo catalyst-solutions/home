2844c697b585781173cc95939d27974d09f9932f
Joshua3212 <joshua-koeck@gmx.de>
2023-07-07 11:55:13 +0200

    a

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-white bg-indigo-600 text rounded-full px-6 py-2 w-max">
+  <div class="text-black bg-neutral-200 text rounded-full px-6 py-2 w-max">
     <slot />
   </div>
 </template>
