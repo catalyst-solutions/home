08a13cde80242b88ac2cb994c29f9435b966caf3
Joshua3212 <joshua-koeck@gmx.de>
2023-08-19 19:20:50 +0200

    a

--- pages/index.vue
+++ pages/index.vue
@@ -97,7 +97,7 @@ const i18n = useI18n();
 <i18n lang="json">
 {
   "de": {
-    "header": "Wir sind eine Design Agentur mit Fokus auf der Entwicklung von digitalen Unternehmensauftritten die Kunden überzeugen",
+    "header": "Wir sind eine Design Agentur mit Fokus auf der Entwicklung von digitalen Unternehmensauftritten, die Kunden überzeugen",
     "csolWay": {
       "heading1": "Gutes Design ist schwer",
       "heading2": "Wir haben Erfahrung!",
