817889138ce0c549146509980a7e96600e6de5db
Joshua3212 <joshua-koeck@gmx.de>
2023-08-26 12:27:46 +0200

    a

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,6 +44,14 @@ useSeoMeta({
 useHead({
   link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 });
+export default {
+  mounted() {
+    console.log(
+      `%cHey 👋\n\nThere's nothing fancy to find here 😅`,
+      "font-size:30px; "
+    );
+  },
+};
 </script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
