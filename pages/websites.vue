50210f8f806782269045ba71141c0de4a5ae1358
noahcfie <contact@csol.dev>
2023-12-31 19:16:54 +0100

    typo

--- pages/websites.vue
+++ pages/websites.vue
@@ -53,7 +53,7 @@ export default {
             title: "Buisness Solo",
             description: "für Einzelunternehmen",
             bulletpoints: [
-              "bis zu 3 Unterseiten inklusive",
+              "bis zu 3 Unterseiten",
               "Schnelle Ladezeiten",
               "Responsive Design",
               "Interaktives Kontaktformular",
